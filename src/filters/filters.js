8629f2a03ee20309c3791c42cb4f8ae0d990f786
Sumit lal verma <47994803+sumitlalverma@users.noreply.github.com>
2023-03-17 16:06:34 +0530

    Update filters.js

--- src/filters/filters.js
+++ src/filters/filters.js
@@ -1,6 +1,6 @@
 import { onFilter } from "..";
 
-export default function renderFilters(items) {
+export default function renderFilters(items, courses) {
   const container = document.createElement("div");
 
   items.forEach((filter) => {
@@ -12,11 +12,12 @@ export default function renderFilters(items) {
     input.type = "checkbox";
     input.id = filter;
 
-    input.addEventListener('change', () => onFilter(filter, input));
+    input.addEventListener("change", () => onFilter(filter, input));
 
+    const itemLength = courses.filter((f) => f.offeredBy === filter);
     const label = document.createElement("label");
     label.setAttribute("for", filter);
-    label.textContent = `${filter}`;
+    label.textContent = `${filter} (${itemLength.length})`;
 
     filterItem.appendChild(input);
     filterItem.appendChild(label);
@@ -26,3 +27,30 @@ export default function renderFilters(items) {
 
   return container;
 }
+
+function toogleArrow(action) {
+  const arrowManager = document.getElementById("arrow-manager");
+  if (action === "open") {
+    arrowManager.classList.replace("fa-chevron-down", "fa-chevron-up");
+  } else {
+    arrowManager.classList.replace("fa-chevron-up", "fa-chevron-down");
+  }
+}
+
+function togglePopup() {
+  const popup = document.getElementById("filter-popup");
+  console.log(`popup.style.display`, popup.style.display);
+  if (popup.style.display === "none" || !popup.style.display) {
+    popup.style.display = "block";
+    toogleArrow("open");
+  } else {
+    popup.style.display = "none";
+    toogleArrow("close");
+  }
+}
+
+const popupToogleButton = document.getElementById("popup-toogle-button");
+popupToogleButton.addEventListener("click", togglePopup);
+
+const crossButton = document.getElementById("cross-popup");
+crossButton.addEventListener("click", togglePopup);
